d28c16552d056ead0de8c8b44ea998fce704031a
DIAKITE Abdoul Oudouss <diakson1441@gmail.com>
2021-10-30 21:25:37 +0100

    Initial Commit

---
+++ Cours1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Cours1
+//
+//  Created by Diakite on 30/10/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

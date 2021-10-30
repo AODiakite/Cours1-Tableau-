0f0cab4a7b91e668daf7ecd7d13ebaccae2c5c10
DIAKITE Abdoul Oudouss <diakson1441@gmail.com>
2021-10-30 22:45:30 +0100

    Update main.c

--- Cours1/main.c
+++ Cours1/main.c
@@ -9,21 +9,29 @@
 
 const int Nmax=20;
 
+
 int main(int argc, const char * argv[]) {
+    //Declaration tableau "tab" de taille Nmax
     float tab[Nmax];
-    int N,i;
-    printf("Donnez la taille du tableau N= ");
-    scanf("%d",&N);
+    int N=30,i; //Declaration de variables
+    do{
+    printf("La taille Max du tableau est de %d\n",Nmax);
+    printf("Donnez la taille du tableau N= "); //Demande de la taille du tableau
+    scanf("%d",&N);//Lecture de la taille du tableau
+    }while(N>20);
     printf("Veillez remplire le tableau\n");
+    //Demande de remplissage du tableau
     for (i=0; i<N;i++) {
         printf("Donner T[%d]= ",i+1);
         scanf("%f",&tab[i]);
     }
-    printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+    printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");//Separateur
     printf("Affichage de tableau :\n");
+    //Affichage du tableau de taille N
     for (i=0; i<N; i++) {
         printf("|| %.1f ",tab[i]);
     }
    
     return 0;
 }
+

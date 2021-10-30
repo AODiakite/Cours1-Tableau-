5ffdf1cd31a6ad4faf8583222b62b316fdb2f8fd
DIAKITE Abdoul Oudouss <diakson1441@gmail.com>
2021-10-30 22:15:36 +0100

    Update main.c

--- Cours1/main.c
+++ Cours1/main.c
@@ -7,8 +7,23 @@
 
 #include <stdio.h>
 
+const int Nmax=20;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    float tab[Nmax];
+    int N,i;
+    printf("Donnez la taille du tableau N= ");
+    scanf("%d",&N);
+    printf("Veillez remplire le tableau\n");
+    for (i=0; i<N;i++) {
+        printf("Donner T[%d]= ",i+1);
+        scanf("%f",&tab[i]);
+    }
+    printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+    printf("Affichage de tableau :\n");
+    for (i=0; i<N; i++) {
+        printf("|| %.1f ",tab[i]);
+    }
+   
     return 0;
 }
